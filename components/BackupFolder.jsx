4e5e61acb2e68da19db4ec7c6edacd7cb552cce0
user <user.inanin@gmail.com>
2024-05-29 17:27:09 +0800

    commit

--- components/BackupFolder.jsx
+++ components/BackupFolder.jsx
@@ -6,16 +6,16 @@ import { BsFolder } from "react-icons/bs"
 const FolderTree = ({ folders }) => {
   return (
     <ul className="ml-3">
-      {folders?.map((folder) => (
-        <div className="flex gap-1">
-          <BsFolder className="text-lg" style={{ marginTop: "2px" }} />
-          <li>
+      {folders?.map((folder, index) => (
+        <li key={index}>
+          <div className="flex gap-1">
+            <BsFolder className="text-lg" style={{ marginTop: "2px" }} />
             <div>{folder.name}</div>
-            {folder.subfolders.folders.length > 0 && (
-              <FolderTree folders={folder.subfolders.folders} />
-            )}
-          </li>
-        </div>
+          </div>
+          {folder.subfolders.folders.length > 0 && (
+            <FolderTree folders={folder.subfolders.folders} />
+          )}
+        </li>
       ))}
     </ul>
   )
@@ -34,7 +34,9 @@ const BackupFolder = ({ server }) => {
 
   return (
     <div className="bg-navy p-5 rounded-3xl mb-5">
-      <h1 className="text-lg mb-3">Folder Structure (D:\backup_202405\daily_{server})</h1>
+      <h1 className="text-lg mb-3">
+        Folder Structure (D:\backup_202405\daily_{server})
+      </h1>
       <FolderTree folders={folderData?.folders} />
     </div>
   )
