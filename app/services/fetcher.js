9ce5d794c5c3ba78ef0d50881f14eae2be87bc65
user <user.inanin@gmail.com>
2024-05-30 09:49:28 +0800

    commit

--- app/services/fetcher.js
+++ app/services/fetcher.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://18.162.206.99/systracker',
+    baseURL: 'http://18.162.206.99/systracker-backend',
 })
 
 const fetcher = (url) => axiosInstance.get(url).then(res => res.data)
