fbd5912443554e11cb60eae35b04438a95249406
user <user.inanin@gmail.com>
2024-06-05 12:35:17 +0800

    commit

--- app/services/queries.js
+++ app/services/queries.js
@@ -1,14 +1,24 @@
-'use client'
-import useSWR from "swr";
+"use client"
+import useSWR from "swr"
 
 export function useDiskSpace() {
-    return useSWR('http://18.162.206.99/systracker-backend/disk-space')
+  return useSWR("http://18.162.206.99/systracker-backend/disk-space")
 }
 
 export function useFolderDetails(server) {
-    return useSWR(`http://18.162.206.99/systracker-backend/folder-details?server=${server}`)
+  return useSWR(
+    `http://18.162.206.99/systracker-backend/folder-details?server=${server}`
+  )
 }
 
 export function useFolderSize(path) {
-    return useSWR(`http://18.162.206.99/systracker-backend/folder-size?path=${path}`)
+  return useSWR(
+    `http://18.162.206.99/systracker-backend/folder-size?path=${path}`
+  )
+}
+
+export function useCountFiles(path) {
+  return useSWR(
+    `http://18.162.206.99/systracker-backend/count-files?path=${path}`
+  )
 }
--- components/DetailModal.jsx
+++ components/DetailModal.jsx
@@ -1,17 +1,27 @@
-import { useFolderSize } from "@/app/services/queries"
+import { useCountFiles, useFolderSize } from "@/app/services/queries"
 import { BsFolder } from "react-icons/bs"
 
 const DetailModal = ({ open, onClose, currentPath, server }) => {
   const folderSizeQuery = useFolderSize(currentPath)
-  const { data, error, isLoading } = folderSizeQuery
+  const {
+    data: folderSizeData,
+    error: folderSizeError,
+    isLoading: folderSizeIsLoading,
+  } = folderSizeQuery
 
-  console.log(data?.folderSize)
+  const countFilesQuery = useCountFiles(currentPath)
+  const {
+    data: countFilesData,
+    error: countFilesError,
+    isLoading: countFilesIsLoading,
+  } = countFilesQuery
+
   return (
     <div
       className={`fixed inset-0 flex justify-center items-center transition-opacity ${
         open ? "opacity-100" : "opacity-0 pointer-events-none"
       }`}
-      style={{zIndex: '999'}}
+      style={{ zIndex: "999" }}
     >
       <div
         className="fixed inset-0 bg-black opacity-50"
@@ -20,20 +30,28 @@ const DetailModal = ({ open, onClose, currentPath, server }) => {
       <div className="absolute bg-navy p-4 rounded-2xl shadow-lg w-80">
         <div className="flex gap-3">
           <BsFolder className="text-3xl mb-5" />
-          <h1>
-            {server} 
-          </h1>
+          <h1>{server}</h1>
         </div>
         <p>Full Path:</p>
         <p>{currentPath}</p>
-        <p>Size:</p>
-        {isLoading ? (
-          <p>Loading data size...</p>
-        ) : (
-          <p>
-            {data?.folderSize} {data?.unit}
-          </p>
-        )}
+        <div className="mt-2">
+          <p>Size:</p>
+          {folderSizeIsLoading ? (
+            <p>Loading data size...</p>
+          ) : (
+            <p>
+              {folderSizeData?.folderSize} {folderSizeData?.unit}
+            </p>
+          )}
+        </div>
+        <div className="mt-2">
+          <p>Contains:</p>
+          {countFilesIsLoading ? (
+            <p>Loading data count...</p>
+          ) : (
+            <p>{countFilesData?.fileCount} files</p>
+          )}
+        </div>
       </div>
     </div>
   )
