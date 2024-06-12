1f235a4048341bb50c03c0aef1e91794327ee77b
Desmond <desmondhei25@gmail.com>
2024-06-13 07:37:17 +0800

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    transpilePackages: ['@mui/x-charts']
+  transpilePackages: ["@mui/x-charts"],
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: "https://bvfycdzxotyzzqtjjogv.supabase.co",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY:
+      eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+        .eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2ZnljZHp4b3R5enpxdGpqb2d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5NzE4NDYsImV4cCI6MjAzMjU0Nzg0Nn0
+        .lug3jeeZyoH5IPWJM2ZzE3Tec3sjYd84V - OeiszWKSk,
+
+    MONGODB_URI:
+      "mongodb+srv://desmond:m1WMi2hhXnJCORr2@cluster0.22fyenu.mongodb.net/systracker",
+    NEXTAUTH_SECRET: asdjadfklafjladfjalfj,
+    NEXTAUTH_URL: "http://localhost:3000",
+  },
 }
 
 export default nextConfig
